6556d8164735250869d274d25dd88cc8cc3b597a
jpgomez <juampi630@gmail.com>
2019-09-20 21:25:27 -0400

    token respsonse

--- contacto/AppContacto/AppContacto/App.xaml.cs
+++ contacto/AppContacto/AppContacto/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppContacto.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace AppContacto
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new ListViewContact();
         }
 
         protected override void OnStart()
