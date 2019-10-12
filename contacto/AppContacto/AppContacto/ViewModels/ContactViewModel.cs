c499b87d450b3c6b3509748ee04d93f67ab4b99d
juampi630@gmail.com <juampi630@gmail.com>
2019-10-11 20:01:18 -0400

    MVVM FASE 2

---
+++ contacto/AppContacto/AppContacto/ViewModels/ContactViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AppContacto.ViewModels
+{
+    using AppContacto.Services;
+
+   public class ContactViewModel:BaseViewModel
+    {
+
+        #region Attributes
+        ApiService apiService;
+        #endregion
+
+
+
+    }
+}
